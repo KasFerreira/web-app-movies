86109b83c6a6919639866345837275d063f7ae9c
Lucas Ferreira <lucas.ferreira@convergenceworks.com.br>
2021-01-04 14:08:48 -0300

    feat: Navbar

--- desafio/src/components/Header/Heading.jsx
+++ desafio/src/components/Header/Heading.jsx
@@ -1,9 +1,29 @@
 import '../Header/heading.css'
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button}from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom'
+
 
 function Heading(){
     return <div className="Heading">
+  <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#home">LucasFilms</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/movies">Movies</Nav.Link>
+      <Nav.Link href="/perfis">Perfis</Nav.Link>
+     
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="search for a movie" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+    <Nav.Link href="/login">Login</Nav.Link>
+     <Nav.Link href="#link">Logout</Nav.Link>
 
-
+  </Navbar.Collapse>
+</Navbar>
     </div>
     
 }
