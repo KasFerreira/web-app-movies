aa61efd4e63cd7720643fec8683aacbf7759c570
Lucas Ferreira <lucas.ferreira@convergenceworks.com.br>
2021-01-07 12:48:01 -0300

    feat: JWT auth and history implementation

--- desafio/src/components/Login/Login.jsx
+++ desafio/src/components/Login/Login.jsx
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import Heading from "../Header/Heading";
 import axios from "axios";
 import '../Login/login.css'
+import { Link } from 'react-router-dom'
+import { Button }from 'react-bootstrap'
+import { setLocalStorage } from '../../helpers/localstorage'
+import { useHistory } from "react-router-dom"
+
+
 
 
 function initialState(){
@@ -10,7 +16,8 @@ function initialState(){
 
 const Login = () => {
   const [values, setValues]= useState(initialState)
-
+  const [isErros, setIsErros] = useState(false)
+  const history = useHistory()
   function onChange(event){
     const {value, name} = event.target
 
@@ -20,7 +27,14 @@ const Login = () => {
 
   function Submit(event){
     event.preventDefault()
-    axios.post("https://myapiteste.free.beeceptor.com",values).then((response)=>console.log(response))
+    axios.post("http://127.0.0.1:3333/login",values).then((response)=> {
+      
+    setLocalStorage("TokenJWT", response.data.token) 
+    history.push('/movies')
+    console.log(response.data.token)
+
+    } ).catch((e)=>{setIsErros(true)})
+
 
   }
 
@@ -29,21 +43,28 @@ const Login = () => {
       
         <Heading/>
         <div className="user-login">
-          <h1 className="user-login-title">Acessar o Sistema</h1>
-          <form onSubmit={Submit} >
+          <h1 className="user-login-title">Acessar</h1>
+
+          <form onSubmit={Submit} className="user-">
             <div className="user-login-form-control">
-              <label>Usuário</label>
-              <input   id="user" type="text" name="user" onChange={onChange}  />
+              <label>Login</label>
+              <input   id="user" type="email" placeholder="email" name="email" onChange={onChange}  />
             
 
 
             </div>
-            <div className="user-login-form-control">
-              <label htmlFor="password">Senha</label>
-              <input  id="password" type="password" name="password" onChange={onChange}  />
+            <div className="user-login-form-contol">
+              <label htmlFor="password">Senha </label>
+              <input  id="password" type="password" placeholder="senha"name="password" onChange={onChange}  />
 
             </div>
-            <button>Enviar</button>
+            <div id="btn-form">
+              
+              <Button type="submit"> Enviar</Button>
+            <Link to="/cadastro">Cadastrar</Link>
+            {isErros&&<div>Login ou senha incorretos!</div>}
+            </div>
+            
 
           </form>
         
