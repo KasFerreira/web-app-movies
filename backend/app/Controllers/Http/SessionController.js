ea6aaf0cacc148758cf7db36ab267e8138dcc4da
Lucas Ferreira <lucas.ferreira@convergenceworks.com.br>
2021-01-07 22:47:53 -0300

    feat: Session controller

--- backend/app/Controllers/Http/SessionController.js
+++ backend/app/Controllers/Http/SessionController.js
@@ -1,14 +1,13 @@
 'use strict'
 
 class SessionController {
+  async create ({ request, auth }) { 
+    const { email, password } = request.all()
 
-    async create ({ request, auth }) { 
-        const { email, password } = request.all()
-    
-        const token = await auth.attempt(email, password)
-        console.log(token)
-        return token
-      }
+    const token = await auth.attempt(email, password)
+
+    return token
+  }
 }
 
 module.exports = SessionController
